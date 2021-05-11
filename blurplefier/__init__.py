0da1f8572eb805f4ef9c77a7b5ca278f953de045
Sonic4999 <sonictbm@gmail.com>
2021-05-11 09:58:35 -0400

    Made repo setup.py friendly

---
+++ blurplefier/__init__.py
@@ -0,0 +1,9 @@
+"""
+blurplefier-standalone
+~~~~~~~
+A standalone version of the blurplefier that the Blurplefier bot has.
+Copyright (c) 2019 - 2021 Project Blurple, Sonic49
+:license: MIT, see LICENSE for more details.
+"""
+from .blurplefier import convert_image
+from .blurplefier import writeImage
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="blurplefier-standalone",
+    version="0.0.1",
+    description="A standalone version of the blurplefier that the Blurplefier bot has.",
+    license="MIT",
+    long_description=long_description,
+    author="Project Blurple, Sonic49",
+    url="https://github.com/Sonic4999/blurplefier-standalone",
+    packages=["blurplefier"],
+    install_requires=["pillow"],
+)
